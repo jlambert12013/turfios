2464a417bb0577d2dd8657b873ed7b0c66a25dc8
Jim Lambert <jlambert12013@gmail.om>
2023-01-04 00:41:55 -0600

    First Commit: Added Firebase Auth and Firestore.

--- Turf/TurfApp.swift
+++ Turf/TurfApp.swift
@@ -3,18 +3,29 @@
 //  Turf
 //
 //  Created by Jim Lambert on 12/28/22.
-//
 // “The only way to do great work is to love what you do” - Steve Jobs
 
+
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
-struct TurfApp: App {
+struct YourApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
-        
     }
 }
