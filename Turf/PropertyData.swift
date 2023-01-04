9b18a45bb77b45626b9e80a5b2f757dffa9edb62
Jim Lambert <jlambert12013@gmail.om>
2023-01-04 00:58:25 -0600

    Second Commit: Cleanup

--- Turf/PropertyData.swift
+++ Turf/PropertyData.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class ProppertyViewModel: ObservableObject {
+class PropertyData: ObservableObject {
     
     @Published var property = []
     
--- Turf/TurfApp.swift
+++ Turf/TurfApp.swift
@@ -18,7 +18,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 @main
 struct YourApp: App {
-    // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
