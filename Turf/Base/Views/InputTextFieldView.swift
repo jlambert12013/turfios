2aba5bc203800038dd3fc0bec7f7f0a814349eea
Jim Lambert <jlambert12013@gmail.om>
2023-01-04 01:50:27 -0600

    Third Commit: Created the Text Field View Component.

---
+++ Turf/Base/Views/InputTextFieldView.swift
@@ -0,0 +1,53 @@
+//
+//  InputTextFieldView.swift
+//  Turf
+//
+//  Created by Jim Lambert on 1/4/23.
+//
+
+import SwiftUI
+
+struct InputTextFieldView: View {
+    
+    @Binding var text: String
+    var placeholder: String
+    var keyboard: UIKeyboardType
+    var icon: String?
+    
+    private let leadingAmount: CGFloat = 30
+    
+    var body: some View {
+        VStack {
+            TextField(placeholder, text: $text)
+                .frame(maxWidth: .infinity, minHeight: 50)
+                .padding(.leading, icon == nil ? leadingAmount / 2 : leadingAmount)
+                .keyboardType(keyboard)
+                .background(
+                    
+                    ZStack(alignment: .leading) {
+                        if let systemImage = icon {
+                            Image(systemName: systemImage)
+                                .font(.system(size: 16, weight: .semibold))
+                                .padding(.leading, 5)
+                                .foregroundColor(Color.gray.opacity(0.3))
+                        }
+                        
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .stroke(Color.gray.opacity(0.2))
+                    }
+                )
+        }
+    }
+}
+
+struct InputTextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputTextFieldView(text: .constant(""),
+                           placeholder: "Email",
+                           keyboard: .emailAddress,
+                           icon: "envelope")
+        .previewLayout(.sizeThatFits)
+        .previewDisplayName("Text Input")
+        .padding()
+    }
+}
