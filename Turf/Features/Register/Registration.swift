67cc56141fc2303bff84f5913d392ebf4f45609f
Jim Lambert <jlambert12013@gmail.om>
2023-01-04 09:38:12 -0600

    Made Registration Model

---
+++ Turf/Features/Register/Registration.swift
@@ -0,0 +1,15 @@
+//
+//  Registration.swift
+//  Turf
+//
+//  Created by Jim Lambert on 1/4/23.
+//
+
+import Foundation
+
+struct Registration{
+    var email: String
+    var password: String
+    var firstName: String
+    var lastName: String
+}
---
+++ Turf/Features/Register/RegistrationService.swift
@@ -0,0 +1,12 @@
+//
+//  RegistrationService.swift
+//  Turf
+//
+//  Created by Jim Lambert on 1/4/23.
+//
+
+import Foundation
+
+protocol RegistrationService {
+    func register(with details: Registration)
+}
