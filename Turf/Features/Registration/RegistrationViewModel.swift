ad6d0253f9a9e58232d0754584c45a946f8aa7a5
Jim Lambert <jlambert12013@gmail.om>
2023-01-05 13:27:20 -0600

    More Cleaning up code.

--- Turf/Features/Registration/RegistrationViewModel.swift
+++ Turf/Features/Registration/RegistrationViewModel.swift
@@ -5,54 +5,52 @@
 //  Created by Jim Lambert on 1/4/23.
 //
 
-import Foundation
 import Combine
+import Foundation
 
 enum RegistrationState {
-    case successful
-    case failed(error: Error)
-    case na
+  case successful
+  case failed(error: Error)
+  case na
 }
 
 protocol RegistrationViewModel {
-    func register()
-    var service: RegistrationService  { get }
-    var state: RegistrationState { get }
-    var userDetails: RegistrationDetails { get }
-    init(service: RegistrationService)
+  func register()
+  var service: RegistrationService { get }
+  var state: RegistrationState { get }
+  var userDetails: RegistrationDetails { get }
+  init(service: RegistrationService)
 }
-
 
 final class RegistrationViewModelImpl: ObservableObject, RegistrationViewModel {
-    
-    let service: RegistrationService
-    var state: RegistrationState = .na
-    var userDetails: RegistrationDetails = RegistrationDetails.new
-    
-    private var subscriptions = Set<AnyCancellable>()
-    
-    init(service: RegistrationService) {
-        self.service = service
-    }
-    
-    
-    
-    func register() {
-        
-        service
-            .register(with: userDetails)
-            .sink { [weak self] res in
-                
-                switch res {
-                case .failure(let error):
-                    self?.state = .failed(error: error)
-                default: break
-                }
-            } receiveValue: { [weak self] in
-                self?.state = .successful
-            }
-            .store(in: &subscriptions)
-        
-    }
-}
 
+  let service: RegistrationService
+
+  var state: RegistrationState = .na
+
+  var userDetails: RegistrationDetails = RegistrationDetails.new
+
+  private var subscriptions = Set<AnyCancellable>()
+
+  init(service: RegistrationService) {
+    self.service = service
+  }
+
+  func register() {
+
+    service
+      .register(with: userDetails)
+      .sink { [weak self] res in
+
+        switch res {
+        case .failure(let error):
+          self?.state = .failed(error: error)
+        default: break
+        }
+      } receiveValue: { [weak self] in
+        self?.state = .successful
+      }
+      .store(in: &subscriptions)
+
+  }
+}
