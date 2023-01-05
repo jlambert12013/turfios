74783e0706c77c8d666ab969ecc35b5b982982b9
Jim Lambert <jlambert12013@gmail.om>
2023-01-05 13:31:24 -0600

    More Cleanup

--- Turf/Features/Registration/RegistrationService.swift
+++ Turf/Features/Registration/RegistrationService.swift
@@ -6,73 +6,82 @@
 //
 
 import Combine
-import Foundation
 import Firebase
 import FirebaseDatabase
+import Foundation
 
-enum Keys: String {     // These are the Keys & Values for storing results in Firebase
-    case fireName
-    case lastName
+enum Keys: String {  // These are the Keys & Values for storing results in Firebase
+  case fireName
+  case lastName
 }
 
 protocol RegistrationService {
-    func register(with details: RegistrationDetails) -> AnyPublisher<Void, Error>
+  func register(with details: RegistrationDetails) -> AnyPublisher<Void, Error>
 }
 
 final class RegistrationServiceImpl: RegistrationService {
-    func register(with details: RegistrationDetails) -> AnyPublisher<Void, Error> {
-        
-        Deferred {
-            
-            Future { promise in
-                
-                Auth.auth()
-                    .createUser(withEmail: details.email,
-                                password:  details.password) { res, error in
-                        
-                        if let err = error {
-                            
-                            promise(.failure(err))
-                            
-                        } else {
-                            
-                            if let uid = res?.user.uid {
-                                
-                                let values = [Keys.fireName.rawValue: details.firstName,
-                                              Keys.lastName.rawValue: details .lastName] as [String: Any]
-                                
-                                Database.database()
-                                    .reference()
-                                    .child("users")
-                                    .child(uid)
-                                    .updateChildValues(values) { error, ref in
-                                        
-                                        if let err = error {
-                                            
-                                            promise(.failure(err))
-                                            
-                                        } else {
-                                            
-                                            promise(.success(()))
-                                            
-                                        }
-                                    }
-                                
-                            } else {
-                                
-                                promise(.failure(NSError(domain: "INVALID USER ID",
-                                                         code: 0,
-                                                         userInfo: nil)))
-                                
-                            }
-                        }
-                        
+  func register(with details: RegistrationDetails) -> AnyPublisher<Void, Error> {
+
+    Deferred {
+
+      Future { promise in
+
+        Auth.auth()
+          .createUser(
+            withEmail: details.email,
+            password: details.password
+          ) { res, error in
+
+            if let err = error {
+
+              promise(.failure(err))
+
+            } else {
+
+              if let uid = res?.user.uid {
+
+                let values =
+                  [
+                    Keys.fireName.rawValue: details.firstName,
+                    Keys.lastName.rawValue: details.lastName,
+                  ] as [String: Any]
+
+                Database.database()
+                  .reference()
+                  .child("users")
+                  .child(uid)
+                  .updateChildValues(values) { error, ref in
+
+                    if let err = error {
+
+                      promise(.failure(err))
+
+                    } else {
+
+                      promise(.success(()))
+
                     }
+                  }
+
+              } else {
+
+                promise(
+                  .failure(
+                    NSError(
+                      domain: "INVALID USER ID",
+                      code: 0,
+                      userInfo: nil)))
+
+              }
             }
-        }
-        .receive(on: RunLoop.main)
-        .eraseToAnyPublisher()
-        
+
+          }
+      }
     }
-    
+    .receive(on: RunLoop.main)
+    .eraseToAnyPublisher()
+
+  }
+
 }
+
