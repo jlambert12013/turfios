7681b46889344571f15827cf093775ac11a0a84d
Jim Lambert <jlambert12013@gmail.com>
2023-01-06 18:52:31 -0600

    Update SessionService.swift
    Fixing Github and Local Repository Issue

--- Turf/Features/Session/Serivce/SessionService.swift
+++ Turf/Features/Session/Serivce/SessionService.swift
@@ -6,69 +6,69 @@
 //
 
 import Combine
-import Foundation
 import FirebaseAuth
-
+import FirebaseDatabase
+import Foundation
 
 enum SessionState {
-    case loggedIn
-    case loggedOut
+  case loggedIn
+  case loggedOut
 }
 
 protocol SessionService {
-    var state: SessionState { get }
-    var userDetails: SessionUserDetails? { get }
-    func logOut()
+  var state: SessionState { get }
+  var userDetails: SessionUserDetails? { get }
+  func logOut()
 }
 
-
-
-
-
-
-// Implementation of Session Service
 final class SessionServiceImpl: ObservableObject, SessionService {
-    
-    @Published var state: SessionState = .loggedOut
-    @Published var userDetails: SessionUserDetails?
-    
-    private var handler: AuthStateDidChangeListenerHandle?
-    
-    init() {
-        setupFirebaseAuthHandler()
-    }
-    
-    func logOut() {
-        
-    }
-    
+
+  @Published var state: SessionState = .loggedOut
+  @Published var userDetails: SessionUserDetails?
+
+  private var handler: AuthStateDidChangeListenerHandle?
+
+  init() { setupFirebaseAuthHandler() }
+
+  func logOut() {
+
+  }
 }
 
-private extension SessionServiceImpl {
-    
-    func setupFirebaseAuthHandler() {
-        
-        handler = Auth.auth().addStateDidChangeListener { [weak self ] res, user in
-            
-            guard let self = self else {
-                return
-            }
-            
-            
-//            if user != nil {
-//                self.state = .loggedIn
-//            } else {
-//                self.state = .loggedOut
-//            }
-            
-            // Shorthand of above if statment
-            self.state = user != nil ? .loggedIn : .loggedOut
+extension SessionServiceImpl {
 
-            
+  func setupFirebaseAuthHandler() {
+    handler = Auth.auth()
+      .addStateDidChangeListener { [weak self] res, user in
+
+        guard let self = self else { return }
+
+        self.state = user != nil ? .loggedIn : .loggedOut
+      }
+  }
+
+  func handleRefresh(with uid: String) {
+    Database.database()
+      .reference()
+      .child("users")
+      .child(uid)
+      .observe(.value) { [weak self] snapshot in
+
+        guard let self = self,
+          let value = snapshot.value as? NSDictionary,
+          let email = value[RegistrationKeys.email.rawValue] as? String,
+          let firstName = value[RegistrationKeys.firstName.rawValue] as? String,
+          let lastName = value[RegistrationKeys.lastName.rawValue] as? String
+        else { return }
+
+        DispatchQueue.main.async {
+          self.userDetails = SessionUserDetails(
+            email: email,
+            firstName: firstName,
+            lastName: lastName)
         }
-        
-    }
-    
+
+      }
+  }
+
 }
-
-
