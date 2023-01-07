1a8c58524914ade046ac656926ed3bc781bef1b7
Jim Lambert <jlambert12013@gmail.om>
2023-01-06 20:48:39 -0600

    Added Environment Object in App and Home View

--- Turf/TurfApp.swift
+++ Turf/TurfApp.swift
@@ -5,30 +5,31 @@
 //  Created by Jim Lambert on 12/28/22.
 // “The only way to do great work is to love what you do” - Steve Jobs
 
-
-import SwiftUI
 import FirebaseCore
+import SwiftUI
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
         FirebaseApp.configure()
         return true
     }
 }
 
-@main
-struct TurfApp: App {
-    
+@main struct TurfApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+    @StateObject var session = SessionServiceImpl()
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                LoginView()
+                switch session.state {
+                case .loggedIn: HomeView().environmentObject(session)
+                case .loggedOut: LoginView()
+                }
             }
         }
     }
 }
+
